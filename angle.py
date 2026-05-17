5e590e1396a4d112a02b502583de0224b30a0d39
wemomok <agorbachev267@gmail.com>
2026-05-17 20:11:12 +0300

    Converted Angle to ScalarMeasurement

--- angle.py
+++ angle.py
@@ -1,14 +1,17 @@
 import numpy as np
 
+from copy import copy
 from collections.abc import Iterable
 
 from constants import GeothonConstants
+from scalar_measurement import ScalarMeasurement
 
 
-def convert_to_rads(input_angle=float or int or Iterable, angle_type=GeothonConstants):
+def convert_to_rads(input_angle=float or int or Iterable,
+                    angle_type=GeothonConstants):
     if not isinstance(input_angle, Iterable) and angle_type == GeothonConstants.DMS or \
             isinstance(input_angle, Iterable) and angle_type != GeothonConstants.DMS:
-        raise AttributeError('Angle type provided doesn\'t match input angle')
+        raise BaseException('Angle type provided doesn\'t match input angle')
 
     if angle_type == GeothonConstants.RAD:
         angle = input_angle
@@ -20,7 +23,7 @@ def convert_to_rads(input_angle=float or int or Iterable, angle_type=GeothonCons
         dms = [*input_angle]
 
         if len(dms) > 3:
-            raise AttributeError('Too many values provided')
+            raise BaseException('Too many values provided')
 
         while len(dms) < 3:
             dms.append(0)
@@ -30,12 +33,13 @@ def convert_to_rads(input_angle=float or int or Iterable, angle_type=GeothonCons
                            (-1 if any(val < 0 for val in dms) else 1))
 
     else:
-        raise AttributeError('Invalid angle type provided')
+        raise BaseException('Invalid angle type provided')
 
     return angle % (2 * np.pi)
 
 
 def convert_rads_to(input_angle=float, angle_type=GeothonConstants):
+    input_angle %= (2 * np.pi)
     if angle_type == GeothonConstants.RAD:
         return input_angle
     elif angle_type == GeothonConstants.DEG:
@@ -49,28 +53,29 @@ def convert_rads_to(input_angle=float, angle_type=GeothonConstants):
         s = (deg - d - m / 60) * 3600
         return d, m, round(s, 10)
 
-    raise AttributeError('Invalid angle type provided')
+    raise BaseException('Invalid angle type provided')
 
 
-def convert(input_angle, input_type=GeothonConstants, output_type=GeothonConstants):
+def convert(input_angle, input_type=GeothonConstants,
+            output_type=GeothonConstants):
     rads = convert_to_rads(input_angle, input_type)
     return convert_rads_to(rads, output_type)
 
 
 class Angle:
-    __angle: float
-    __tolerance: float
+    __angle: ScalarMeasurement
 
     angle_type: GeothonConstants
     tolerance_type: GeothonConstants
 
     def __init__(self, input_angle=float or int or Iterable,
-                 angle_type=GeothonConstants.RAD, tolerance=(0,), tolerance_type=GeothonConstants.DMS):
+                 angle_type=GeothonConstants.RAD, tolerance=(0,),
+                 tolerance_type=GeothonConstants.DMS):
         self.angle_type = angle_type
         self.tolerance_type = tolerance_type
 
-        self.__angle = convert_to_rads(input_angle, angle_type)
-        self.__tolerance = convert_to_rads(tolerance, tolerance_type)
+        self.__angle = ScalarMeasurement(convert_to_rads(
+            input_angle, angle_type), convert_to_rads(tolerance, tolerance_type))
 
     def set(self, input_angle=float or int or Iterable, angle_type=None, tolerance=(0,), tolerance_type=None):
         if not angle_type:
@@ -78,20 +83,21 @@ class Angle:
         if not tolerance_type:
             tolerance_type = self.tolerance_type
 
-        self.__angle = convert_to_rads(input_angle, angle_type)
-        self.__tolerance = convert_to_rads(tolerance, tolerance_type)
+        self.__angle = ScalarMeasurement(convert_to_rads(
+            input_angle, angle_type), convert_to_rads(tolerance, tolerance_type))
 
     def get_angle(self):
-        return convert_rads_to(self.__angle, self.angle_type)
+        return convert_rads_to(self.__angle.value, self.angle_type)
 
     def get_tolerance(self):
-        return convert_rads_to(self.__tolerance, self.tolerance_type)
+        return convert_rads_to(self.__angle.tolerance, self.tolerance_type)
 
     def __iadd__(self, other):
         if not isinstance(other, Angle):
             raise BaseException(
                 f"No overload for adding Angle and {type(other)}")
         self.__angle += other.__angle
+        self.__angle.value %= (2 * np.pi)
         return self
 
     def __isub__(self, other):
@@ -99,16 +105,55 @@ class Angle:
             raise BaseException(
                 f"No overload for subtracting Angle and {type(other)}")
         self.__angle -= other.__angle
+        self.__angle.value %= (2 * np.pi)
         return self
 
     def __add__(self, other):
         if not isinstance(other, Angle):
             raise BaseException(
                 f"No overload for adding Angle and {type(other)}")
-        return Angle(self.__angle + other.__angle)
+        result = self.__angle + other.__angle
+        result.value %= (2 * np.pi)
+        result_angle = Angle(
+            result.value, tolerance=result.tolerance, tolerance_type=GeothonConstants.RAD)
+        result_angle.angle_type = self.angle_type
+        result_angle.tolerance_type = self.tolerance_type
+        return result_angle
 
     def __sub__(self, other):
         if not isinstance(other, Angle):
             raise BaseException(
                 f"No overload for subtracting Angle and {type(other)}")
-        return Angle(self.__angle - other.__angle)
+        result = self.__angle - other.__angle
+        result.value %= (2 * np.pi)
+        result_angle = Angle(
+            result.value, tolerance=result.tolerance, tolerance_type=GeothonConstants.RAD)
+        result_angle.angle_type = self.angle_type
+        result_angle.tolerance_type = self.tolerance_type
+        return result_angle
+
+    def __mul__(self, other):
+        result = copy(self)
+        result.__angle *= other
+        result.__angle.value %= (2 * np.pi)
+        return result
+
+    def __truediv__(self, other):
+        result = copy(self)
+        result.__angle /= other
+        result.__angle.value %= (2 * np.pi)
+        return result
+
+    def __imul__(self, other):
+        self = self * other
+        return self
+
+    def __itruediv__(self, other):
+        self = self / other
+        return self
+
+    def __str__(self):
+        return f'{str(self.get_angle())}±{str(self.get_tolerance())}'
+
+    def __repr__(self):
+        return f'Angle: {str(self.get_angle())}±{str(self.get_tolerance())}'
--- scalar_measurement.py
+++ scalar_measurement.py
@@ -1,5 +1,7 @@
 import numpy as np
 
+from copy import copy
+
 
 class ScalarMeasurement:
     def __init__(self, value, tolerance=0):
@@ -19,21 +21,11 @@ class ScalarMeasurement:
         return ScalarMeasurement(self.value - other, self.tolerance)
 
     def __iadd__(self, other):
-        if isinstance(other, ScalarMeasurement):
-            self.value += other.value
-            self.tolerance = np.sqrt(
-                self.tolerance ** 2 + other.tolerance ** 2)
-        else:
-            self.value += other
+        self = self + other
         return self
 
     def __isub__(self, other):
-        if isinstance(other, ScalarMeasurement):
-            self.value -= other.value
-            self.tolerance = np.sqrt(
-                self.tolerance ** 2 + other.tolerance ** 2)
-        else:
-            self.value -= other
+        self = self - other
         return self
 
     def __mul__(self, other):
@@ -48,11 +40,19 @@ class ScalarMeasurement:
                 (self.tolerance / other.value) ** 2 + (other.tolerance / self.value) ** 2))
         return ScalarMeasurement(self.value / other, self.tolerance / other)
 
+    def __imul__(self, other):
+        self = self * other
+        return self
+
+    def __itruediv__(self, other):
+        self = self / other
+        return self
+
     def __pow__(self, n):
         if not isinstance(n, int):
             raise BaseException(f'Raising ScalarMeasurement to {
                                 type(n)} is not supported')
-        result = self
+        result = copy(self)
         for _ in range(n):
             result *= self
 
@@ -62,7 +62,7 @@ class ScalarMeasurement:
         if not isinstance(n, int):
             raise BaseException(f'Raising ScalarMeasurement to {
                                 type(n)} is not supported')
-        initial = self
+        initial = copy(self)
         for _ in range(n):
             self *= initial
 
@@ -102,4 +102,4 @@ class ScalarMeasurement:
         return f'{self.value}±{self.tolerance}'
 
     def __repr__(self):
-        return f'ScalarMeasurement {self.value}±{self.tolerance}'
+        return f'ScalarMeasurement: {self.value}±{self.tolerance}'
